1fdc0f4ef4072bab1407bade766918819015f7f5
Michael Salaverry <michaels@axonize.com>
2019-08-05 13:18:15 +0300

    feat: 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/main.scss';
+
+const LostPage = () => {
+    return (
+        <div>
+            <h1>You seem a bit lost.</h1>
+            <a href="/">Let's go back home</a>
+        </div>
+    )
+}
+
+export default LostPage;
