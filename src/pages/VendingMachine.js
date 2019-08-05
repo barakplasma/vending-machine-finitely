d13f46165c795a5cceda584a53384badde9ef573
Michael Salaverry <michaels@axonize.com>
2019-08-05 13:52:14 +0300

    feat: added keypad

--- src/pages/VendingMachine.js
+++ src/pages/VendingMachine.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types'
 
-const Product = ({label, icon, price = 0}) => (<div role="img" aria-label={label}>
-{icon}
+const Product = ({label, icon, price = 0}) => (<div><span role="img" aria-label={label}>{icon}</span>
 <br/>
 {label}
 <br/>
-{price}
-</div>)
+<span>price: {price}</span>
+</div>
+)
+
+const Keypad = () => {
+  const [addAmount, setAmount] = useState(1);
+  const [wallet, setWallet] = useState(1);
+  return (<div className="keypad">
+  <span className="wallet">{wallet}</span>
+  <br/>
+  <br/>
+  <input type="number" min="0.00" max="100.00" step="1" onChange={e=>setAmount(parseInt(e.currentTarget.value, 10))}/>
+  <button onClick={() => {
+    setWallet(wallet + addAmount);
+  }}>Add {addAmount}</button>
+  </div>)
+}
 
 function VendingMachine() {
   const [inventory, setInventory] = useState({    
@@ -25,17 +39,17 @@ function VendingMachine() {
     }
     getVendingMachineInventory();
   }, []);
-  console.log(inventory);
   return (
       <div className="vending-machine">
         <div className="product-rack banana"><Product label='banana' icon='🍌' price={inventory.banana.price}/></div>
         <div className="product-rack apple"><Product label='apple' icon='🍎' price={inventory.apple.price}/></div>
         <div className="product-rack toy-car"><Product label='toy-car' icon='🚙' price={inventory['toy-car'].price}/></div>
+        <div className="logo">MS</div>
         <div className="product-rack beer"><Product label='beer' icon='🍺' price={inventory.beer.price}/></div>
         <div className="product-rack shoe"><Product label='shoe' icon='👠' price={inventory.shoe.price}/></div>
         <div className="product-rack empty-rack"><Product label='empty-rack' icon='❓'/></div>
-        <div className="keypad"></div>
-        <div className="dispenser">dispenser tray</div>
+        <Keypad/>
+        <div className="dispenser">→ Push Here ←</div>
       </div>
   );
 }
